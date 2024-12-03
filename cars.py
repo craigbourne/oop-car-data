844388e9105a7a888352202c420d22684b6bb0f8
Craig Bourne <craigbourne15@gmail.com>
2024-12-03 11:08:47 +0000

    Add basic Car class structure with empty dictionary

--- cars.py
+++ cars.py
@@ -1,1 +1,10 @@
-print("hello world!")
+class Car:
+    def __init__(self):
+        # Initialise Car class with an empty dictionary
+        self.car_data = {}
+
+def main():
+    car_system = Car()
+
+if __name__ == "__main__":
+    main()
